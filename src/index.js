e52ff042806e19871171e0a61b20b0b7cafa469e
Decay_Destructor <aaryanmantri29@gmail.com>
2024-02-09 02:24:48 +0530

    synthetic funnctions in reactjs

--- src/index.js
+++ src/index.js
@@ -4,52 +4,66 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const books = [
+  {
+    author: 'Krisin Hannah',
+    title: 'The Women: A Novel',
+    img: 'https://m.media-amazon.com/images/I/81oydfRzeBL._AC_UY327_FMwebp_QL65_.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://m.media-amazon.com/images/I/51-78lFnmbL._SY445_SX342_.jpg',
+    id: 2,
+  },
+]
 
-const firstBook = {
-  author: 'Krisin Hannah',
-  title: 'The Women: A Novel',
-  img: 'https://m.media-amazon.com/images/I/81oydfRzeBL._AC_UY327_FMwebp_QL65_.jpg',
-}
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://m.media-amazon.com/images/I/51-78lFnmbL._SY445_SX342_.jpg',
-}
 const Booklist = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      ></Book>
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      ></Book>
+      <EventExamples />
+      {books.map((book) => {
+        return <Book {...book} key={book.id}></Book>
+      })}
     </section>
   )
 }
-const Book = ({ img, title, author, children }) => {
+const Book = ({ img, title, author }) => {
+  console.log(img, author, title)
   return (
     <article className="book">
       <img className="image" src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
-// const Image = () => {
-
-//   return <img className="image" src={img} alt={title} />
-// }
-// const Title = () => {
-//   return <h2>{title}</h2>
-// }
-
-// const Author = () => {
-//   return <h4>{author}</h4>
-// }
+const EventExamples = () => {
+  const handleFormInput = (e) => {
+    // console.log(e.target.value)
+    console.log('handle form input')
+  }
+  const handleButtonClick = (e) => {
+    alert(e.target.value)
+  }
+  const handleFormSubmission = (e) => {
+    e.preventDefault()
+    console.log('form submitted')
+  }
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  )
+}
 root.render(<Booklist />)
